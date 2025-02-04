ed85901114df81950b24d93ad36a62f3ca62ad7f
Hs293Go <hellston20a@gmail.com>
2025-02-04 12:49:16 -0500

    Add treesitter config

---
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+    -- Other plugins...
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "markdown", "cpp", "python", "lua", "vim", "bash" },
+                highlight = {
+                    enable = true,
+                },
+                incremental_selection = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                },
+            })
+        end,
+    }
+}
