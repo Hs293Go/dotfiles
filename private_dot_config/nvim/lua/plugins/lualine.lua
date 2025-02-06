a640f845c5168890905fba63a12d45766290258f
Hs293Go <hellston20a@gmail.com>
2025-02-06 01:06:21 -0500

    Fix background transparency

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -43,7 +43,7 @@ return {
         }
         vim.g.sonokai_style = 'espresso'
         vim.g.sonokai_better_performance = 1
-
+        vim.g.sonokai_transparent_background = 1
         vim.cmd('colorscheme sonokai')
     end
 }
