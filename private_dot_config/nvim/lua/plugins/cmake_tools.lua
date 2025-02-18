10ee9564c3def531ba493f02644bb15f8d3fdadd
Hs293Go <hellston20a@gmail.com>
2025-02-18 16:00:17 -0500

    CMake tools keybindings

--- private_dot_config/nvim/lua/plugins/cmake_tools.lua
+++ private_dot_config/nvim/lua/plugins/cmake_tools.lua
@@ -4,7 +4,7 @@ return {
     local cmake_tools = require("cmake-tools")
     cmake_tools.setup {}
 
-    vim.api.nvim_set_keymap('n', '<F7>', cmake_tools.CMakeBuild, { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<F7>', ':CMakeBuild<CR>', { noremap = true, silent = true })
   end,
 
 }
