dfa7eb6851dfa13f6d1903bb88bb6fd347c0fa02
Hs293Go <hellston20a@gmail.com>
2025-02-20 04:09:41 -0500

    Add cmake-tools options

--- private_dot_config/nvim/lua/plugins/cmake_tools.lua
+++ private_dot_config/nvim/lua/plugins/cmake_tools.lua
@@ -1,10 +1,17 @@
 return {
-  'Civitasv/cmake-tools.nvim',
-  config = function()
-    local cmake_tools = require("cmake-tools")
-    cmake_tools.setup {}
+    'Civitasv/cmake-tools.nvim',
+    config = function()
+        local cmake_tools = require("cmake-tools")
+        cmake_tools.setup {
+            opts = {
+                cmake_generate_options = {"-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1"}
+            }
 
-    vim.api.nvim_set_keymap('n', '<F7>', ':CMakeBuild<CR>', { noremap = true, silent = true })
-  end,
+        }
+        vim.api.nvim_set_keymap('n', '<F7>', ':CMakeBuild<CR>', {
+            noremap = true,
+            silent = true
+        })
+    end
 
 }
