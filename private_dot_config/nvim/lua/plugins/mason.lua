9e5806ffea58e544149854652e7ac218bcdf45c3
Hs293Go <hellston20a@gmail.com>
2025-03-04 15:31:34 -0500

    Update mason-based tool autoinstallation

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -1,15 +1,20 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {"williamboman/mason-lspconfig.nvim"},
-    opts = {
-        ensure_installed =  {"cmakelang", "cmakelint", "stylua", "shfmt"}
-    },
-    config = function()
-        require("mason").setup()
-        require('mason-lspconfig').setup({
-            ensure_installed = {"clangd", "texlab", "pylsp", "cmake"},
+	"williamboman/mason.nvim",
+	dependencies = { "williamboman/mason-lspconfig.nvim", "WhoIsSethDaniel/mason-tool-installer.nvim" },
+	opts = {
+		ensure_installed = { "cmakelang", "cmakelint", "stylua", "shfmt" },
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "clangd", "texlab", "pylsp", "cmake" },
+			automatic_installation = true,
+		})
 
-            automatic_installation = true
-        })
-    end
+		require("mason-tool-installer").setup({
+			ensure_installed = { "cmakelang", "cmakelint", "stylua", "shfmt" },
+			auto_update = true,
+			run_on_start = true,
+		})
+	end,
 }
