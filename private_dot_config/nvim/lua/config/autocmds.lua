1812b49424fa7a15a67586b0a96dddad5734e808
Hs293Go <hellston20a@gmail.com>
2025-03-06 17:09:05 -0500

    Disable unused python autocmd

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -1,16 +1,11 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "cpp", "c" },
-  command = "setlocal ts=2 sw=2 sts=2 expandtab"
+	pattern = { "cpp", "c" },
+	command = "setlocal ts=2 sw=2 sts=2 expandtab",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  command = "setlocal foldmethod=indent makeprg=python\\ %"
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "tex",
-  callback = function()
-    vim.api.nvim_set_keymap('n', '<F5>', ':VimtexCompile<CR>', { noremap = true, silent = true })
-  end
+	pattern = "tex",
+	callback = function()
+		vim.api.nvim_set_keymap("n", "<F5>", ":VimtexCompile<CR>", { noremap = true, silent = true })
+	end,
 })
