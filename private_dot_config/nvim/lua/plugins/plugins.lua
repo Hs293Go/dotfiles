978a555de94a281dd714a60c9ee9e7c0061df0d8
H S Helson Go <hellston20a@gmail.com>
2025-03-12 16:30:06 -0400

    Retire telescope in favor of Lazyvim builtin fzf-lua

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -29,32 +29,4 @@ return {
             { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         },
 	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					layout_config = {
-						prompt_position = "top",
-						preview_width = 0.5,
-					},
-				},
-			})
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {
-				desc = "Telescope find files",
-			})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
-				desc = "Telescope live grep",
-			})
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {
-				desc = "Telescope buffers",
-			})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
-				desc = "Telescope help tags",
-			})
-		end,
-	},
 }
