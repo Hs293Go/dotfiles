155dbba23aa7e603730e977a1a5b0df1d7114461
H S Helson Go <hellston20a@gmail.com>
2025-03-12 22:36:18 -0400

    Switch to catppuccin

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,46 @@
 return {
-    "sainnhe/sonokai",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        vim.g.sonokai_style = 'espresso'
-        vim.g.sonokai_better_performance = 1
-        vim.g.sonokai_transparent_background = 1
-    end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		flavor = "mocha",
+		integrations = {
+			aerial = true,
+			alpha = true,
+			cmp = true,
+			dashboard = true,
+			flash = true,
+			fzf = true,
+			grug_far = true,
+			gitsigns = true,
+			headlines = true,
+			illuminate = true,
+			indent_blankline = { enabled = true },
+			leap = true,
+			lsp_trouble = true,
+			mason = true,
+			markdown = true,
+			mini = true,
+			native_lsp = {
+				enabled = true,
+				underlines = {
+					errors = { "undercurl" },
+					hints = { "undercurl" },
+					warnings = { "undercurl" },
+					information = { "undercurl" },
+				},
+			},
+			navic = { enabled = true, custom_bg = "lualine" },
+			neotest = true,
+			neotree = true,
+			noice = true,
+			notify = true,
+			semantic_tokens = true,
+			snacks = true,
+			telescope = true,
+			treesitter = true,
+			treesitter_context = true,
+			which_key = true,
+		},
+	},
 }
