2842a1f280c83b712ff8fcd24ceb5ff6b079a88c
H S Helson Go <hellston20a@gmail.com>
2025-03-28 00:30:13 -0400

    Retire unused keyaps

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -3,13 +3,6 @@ vim.api.nvim_set_keymap("v", ";", ":", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "::", ";", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "::", ";", { noremap = true, silent = true })
 
--- increment number (plus)
-vim.api.nvim_set_keymap("n", "<c-p>", "<c-a>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<c-p>", "<c-a>", { noremap = true, silent = true })
--- decrement number (minus)
-vim.api.nvim_set_keymap("n", "<c-m>", "<c-x>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<c-m>", "<c-x>", { noremap = true, silent = true })
-
 vim.api.nvim_set_keymap("i", "<F9>", "<C-O>za", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<F9>", "za", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("o", "<F9>", "<C-C>za", { noremap = true, silent = true })
@@ -17,15 +10,6 @@ vim.api.nvim_set_keymap("v", "<F9>", "zf", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprev<CR>", { noremap = true, silent = true })
-
--- show spaces
-vim.api.nvim_set_keymap("n", "<leader>S", ":set nolist!<CR>", { silent = true })
--- show line numbers
-vim.api.nvim_set_keymap("n", "<leader>l", ":set nonu!<CR>", { silent = true })
--- wrap lines
-vim.api.nvim_set_keymap("n", "<leader>w", ":set nowrap!<CR>", { silent = true })
--- hide highlight of searches
-vim.api.nvim_set_keymap("n", "//", ":nohlsearch<CR>", { silent = true })
 
 -- Remap floating terminal to <C-`> for vscode compatibility
 local toggle_floating_terminal = function()
