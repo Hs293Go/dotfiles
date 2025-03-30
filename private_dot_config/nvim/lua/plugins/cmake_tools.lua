dddf3d6f7d818ab1079f8e8748c43ff5d04d8a48
H S Helson Go <hellston20a@gmail.com>
2025-03-30 10:04:28 +0400

    Add cmake keybinds

--- private_dot_config/nvim/lua/plugins/cmake_tools.lua
+++ private_dot_config/nvim/lua/plugins/cmake_tools.lua
@@ -11,5 +11,47 @@ return {
 			noremap = true,
 			silent = true,
 		})
+
+		vim.api.nvim_set_keymap("n", "<leader>Cg", ":CMakeBuild<CR>", {
+			desc = "Generate CMake project",
+			noremap = true,
+			silent = true,
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>Cb", ":CMakeBuild<CR>", {
+			desc = "Build CMake project",
+			noremap = true,
+			silent = true,
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>Ci", ":CMakeInstall<CR>", {
+			desc = "Install CMake project",
+			noremap = true,
+			silent = true,
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>Cx", ":CMakeStopExecutor<CR>", {
+			desc = "Stop CMake Executor",
+			noremap = true,
+			silent = true,
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>Csc", ":CMakeSelectBuildPreset<CR>", {
+			desc = "Select CMake configure preset",
+			noremap = true,
+			silent = true,
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>Csp", ":CMakeSelectBuildPreset<CR>", {
+			desc = "Select CMake build preset",
+			noremap = true,
+			silent = true,
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>Csb", ":CMakeSelectBuildType<CR>", {
+			desc = "Select CMake build type",
+			noremap = true,
+			silent = true,
+		})
 	end,
 }
