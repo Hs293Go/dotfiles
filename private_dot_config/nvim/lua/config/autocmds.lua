7e5fb1642eaf7743ad8199da46eaf97de1183c82
H S Helson Go <hellston20a@gmail.com>
2025-04-03 15:51:18 +0400

    Make XML indented 2 spaces

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -4,6 +4,11 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "xml" },
+	command = "setlocal ts=2 sw=2 sts=2 expandtab",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
 	pattern = "tex",
 	callback = function()
 		vim.api.nvim_set_keymap("n", "<F5>", ":VimtexCompile<CR>", { noremap = true, silent = true })
