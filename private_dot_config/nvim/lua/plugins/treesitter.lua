d7cfef082f18fef3b7a4b17a461535648ee8633c
H S Helson Go <hellston20a@gmail.com>
2025-11-11 04:47:43 -0500

    Streamline treesitter cfg

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,52 +1,47 @@
 return {
-	-- Other plugins...
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					-- systems languages
-					"c",
-					"cpp",
-					"cuda",
-					"rust",
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			-- systems languages
+			"c",
+			"cpp",
+			"cuda",
+			"rust",
 
-					-- scripting languages
-					"python",
-					"lua",
-					"vim",
+			-- scripting languages
+			"python",
+			"lua",
+			"vim",
 
-					-- OS/system/build scripting
-					"bash",
-					"nix",
-					"make",
-					"cmake",
-					"dockerfile",
+			-- OS/system/build scripting
+			"bash",
+			"nix",
+			"make",
+			"cmake",
+			"dockerfile",
 
-					-- markup languages
-					"latex",
-					"markdown",
+			-- markup languages
+			"latex",
+			"markdown",
 
-					-- config languages
-					"toml",
-					"xml",
-					"json",
-					"yaml",
+			-- config languages
+			"toml",
+			"xml",
+			"json",
+			"yaml",
 
-					-- version control
-					"gitignore",
-					"diff",
-				},
-				highlight = {
-					enable = true,
-				},
-				incremental_selection = {
-					enable = true,
-				},
-				indent = {
-					enable = true,
-				},
-			})
-		end,
+			-- version control
+			"gitignore",
+			"diff",
+		},
+		highlight = {
+			enable = true,
+		},
+		incremental_selection = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+		},
 	},
 }
