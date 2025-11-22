299ccfb7ebd765235b301d5a7f574b6d7a1cb35b
H S Helson Go <hellston20a@gmail.com>
2025-11-22 18:57:45 -0500

    Vim: Use lazyvim keybind definition section

--- private_dot_config/nvim/lua/plugins/cmake_tools.lua
+++ private_dot_config/nvim/lua/plugins/cmake_tools.lua
@@ -1,79 +1,74 @@
 return {
 	"Civitasv/cmake-tools.nvim",
 	ft = { "cmake", "cmake.in", "CMakeLists.txt", "cpp", "c", "h", "hpp" },
-	requires = {
-		"folke/which-key.nvim",
-	},
 	lazy = true,
 	opts = {},
+	keys = {
+		{
+			"<leader>Cg",
+			":CMakeBuild<CR>",
+			desc = "Generate CMake project",
+		},
+		{
+			"<leader>Cb",
+			":CMakeBuild<CR>",
+			desc = "Build CMake project",
+		},
+		{
+			"<leader>Ci",
+			":CMakeInstall<CR>",
+			desc = "Install CMake project",
+		},
+		{
+			"<leader>Cd",
+			":CMakeDebug<CR>",
+			desc = "Debug CMake target",
+		},
+		{
+			"<leader>Cr",
+			":CMakeRun<CR>",
+			desc = "Run CMake target",
+		},
+		{
+			"<leader>Cxe",
+			":CMakeStopExecutor<CR>",
+			desc = "Stop CMake Executor",
+		},
+		{
+			"<leader>Cxr",
+			":CMakeStopRunner<CR>",
+			desc = "Stop CMake Runner",
+		},
+		{
+			"<leader>Csc",
+			":CMakeSelectBuildPreset<CR>",
+			desc = "Select CMake configure preset",
+		},
+		{
+			"<leader>Csp",
+			":CMakeSelectBuildPreset<CR>",
+			desc = "Select CMake build preset",
+		},
+		{
+			"<leader>Csb",
+			":CMakeSelectBuildType<CR>",
+			desc = "Select CMake build type",
+		},
+		{
+			"<leader>Cst",
+			":CMakeSelectBuildTarget<CR>",
+			desc = "Select CMake build target",
+		},
+		{
+			"<leader>Csl",
+			":CMakeSelectLaunchTarget<CR>",
+			desc = "Select CMake launch target",
+		},
+	},
 	config = function()
 		local cmake_tools = require("cmake-tools")
 		cmake_tools.setup({
 			cmake_build_directory = "build",
-		})
-		local wk = require("which-key")
-		wk.add({
-			{ "<leader>C", group = "CMake Tools" },
-			{
-				"<leader>Cg",
-				":CMakeBuild<CR>",
-				desc = "Generate CMake project",
-			},
-			{
-				"<leader>Cb",
-				":CMakeBuild<CR>",
-				desc = "Build CMake project",
-			},
-			{
-				"<leader>Ci",
-				":CMakeInstall<CR>",
-				desc = "Install CMake project",
-			},
-			{
-				"<leader>Cd",
-				":CMakeDebug<CR>",
-				desc = "Debug CMake target",
-			},
-			{
-				"<leader>Cr",
-				":CMakeRun<CR>",
-				desc = "Run CMake target",
-			},
-			{
-				"<leader>Cxe",
-				":CMakeStopExecutor<CR>",
-				desc = "Stop CMake Executor",
-			},
-			{
-				"<leader>Cxr",
-				":CMakeStopRunner<CR>",
-				desc = "Stop CMake Runner",
-			},
-			{
-				"<leader>Csc",
-				":CMakeSelectBuildPreset<CR>",
-				desc = "Select CMake configure preset",
-			},
-			{
-				"<leader>Csp",
-				":CMakeSelectBuildPreset<CR>",
-				desc = "Select CMake build preset",
-			},
-			{
-				"<leader>Csb",
-				":CMakeSelectBuildType<CR>",
-				desc = "Select CMake build type",
-			},
-			{
-				"<leader>Cst",
-				":CMakeSelectBuildTarget<CR>",
-				desc = "Select CMake build target",
-			},
-			{
-				"<leader>Csl",
-				":CMakeSelectLaunchTarget<CR>",
-				desc = "Select CMake launch target",
-			},
 		})
 	end,
 }
--- private_dot_config/nvim/lua/plugins/vimtex.lua
+++ private_dot_config/nvim/lua/plugins/vimtex.lua
@@ -5,6 +5,38 @@ return {
 	},
 	ft = "tex", -- Only load for .tex files
 	lazy = false,
+	keys = {
+		{
+			"<leader>Tcc",
+			":VimtexCompile<CR>",
+			desc = "Compile LaTeX with Vimtex",
+		},
+		{
+			"<leader>Tcl",
+			":VimtexClean<CR>",
+			desc = "Clean LaTeX with Vimtex",
+		},
+		{
+			"<leader>Tcv",
+			":VimtexView<CR>",
+			desc = "View PDF with Vimtex",
+		},
+		{
+			"<leader>Twc",
+			":VimtexCountWords<CR>",
+			desc = "Count words with Vimtex",
+		},
+		{
+			"<leader>Ti",
+			":VimtexInfo<CR>",
+			desc = "Vimtex Information",
+		},
+		{
+			"<leader>Te",
+			":VimtexErrors<CR>",
+			desc = "Vimtex errors",
+		},
+	},
 	config = function()
 		vim.g.vimtex_view_general_viewer = "okular"
 		vim.g.vimtex_compiler_method = "latexmk"
@@ -16,39 +48,5 @@ return {
 		vim.g.vimtex_complete_close_braces = 1 -- Automatically close braces
 		vim.g.vimtex_complete_ignore_case = 1 -- Ignore case in completions
 		vim.g.vimtex_complete_smart_case = 1 -- Smart case sensitivity
-		local wk = require("which-key")
-		wk.add({
-			{ "<leader>T", group = "Vimtex" },
-			{
-				"<leader>Tcc",
-				":VimtexCompile<CR>",
-				desc = "Compile LaTeX with Vimtex",
-			},
-			{
-				"<leader>Tcl",
-				":VimtexClean<CR>",
-				desc = "Clean LaTeX with Vimtex",
-			},
-			{
-				"<leader>Tcv",
-				":VimtexView<CR>",
-				desc = "View PDF with Vimtex",
-			},
-			{
-				"<leader>Twc",
-				":VimtexCountWords<CR>",
-				desc = "Count words with Vimtex",
-			},
-			{
-				"<leader>Ti",
-				":VimtexInfo<CR>",
-				desc = "Vimtex Information",
-			},
-			{
-				"<leader>Te",
-				":VimtexErrors<CR>",
-				desc = "Vimtex errors",
-			},
-		})
 	end,
 }
