1571e11bdd45cc91b973a1360eaef48caa465103
H S Helson Go <hellston20a@gmail.com>
2025-12-29 04:20:52 -0500

    Nvim: Disable flash completely
    Nvim: Delete extraneous tree-sitter dependency declaration

--- private_dot_config/nvim/lua/plugins/plugins.lua
+++ private_dot_config/nvim/lua/plugins/plugins.lua
@@ -38,18 +38,6 @@ return {
 	},
 	{
 		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
-        -- stylua: ignore
-        keys = {
-            { "s" , mode = { "n", "x", "o" }, false }, -- https://www.reddit.com/r/neovim/comments/177qsem/disable_flashnvim_default_mapping_in_lazyvim/
-            { "S" , mode = { "n", "x", "o" }, false },
-            { "gs", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            { "gS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-        },
+		disable = true,
 	},
 }
