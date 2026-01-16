a0dfe281a791643be349ba599fa1b36fa6487ccd
H S Helson Go <hellston20a@gmail.com>
2026-01-16 15:31:11 -0500

    Nvim: Update tabline width

--- private_dot_config/nvim/lua/plugins/appearance.lua
+++ private_dot_config/nvim/lua/plugins/appearance.lua
@@ -16,7 +16,13 @@ return {
 			component_separators = { left = "", right = "" },
 			section_separators = { left = "", right = "" },
 			tabline = {
-				lualine_a = { { "tabs", mode = 2 } }, -- mode=2 shows tab names if set
+				lualine_a = {
+					{
+						"tabs",
+						max_length = 88, -- follow black's rationale on reasonable width
+						mode = 2, -- mode=2 shows tab names if set
+					},
+				},
 				lualine_z = {}, -- keep it clean (optional)
 			},
 		},
